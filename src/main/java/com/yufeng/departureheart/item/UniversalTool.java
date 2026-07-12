5396a6f0fd71dbc11edece8bc07136351b573d29
AYuFengRXingA <3453289353@qq.com>
2026-07-12 20:07:26 +0800

    first 1

--- src/main/java/com/yufeng/departureheart/item/UniversalTool.java
+++ src/main/java/com/yufeng/departureheart/item/UniversalTool.java
@@ -4,6 +4,7 @@ import net.minecraft.tags.BlockTags;
 import net.minecraft.world.item.DiggerItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
@@ -19,23 +20,23 @@ public class UniversalTool extends DiggerItem {
 
     @Override
     public float getDestroySpeed(ItemStack stack, BlockState state) {
+        return this.getTier().getSpeed()*2f;
 
-        // 让基岩也有挖掘速度（现阶段基岩无法被破坏）
-        if (state.getBlock() == Blocks.BEDROCK) {
-            return this.getTier().getSpeed();
-        }
 
-        if (state.is(BlockTags.MINEABLE_WITH_PICKAXE) ||
+       /* if (state.is(BlockTags.MINEABLE_WITH_PICKAXE) ||
                 state.is(BlockTags.MINEABLE_WITH_AXE) ||
                 state.is(BlockTags.MINEABLE_WITH_SHOVEL) ||
                 state.is(BlockTags.MINEABLE_WITH_HOE)) {
-            return this.getTier().getSpeed();  // 使用工具本身的挖掘速度
-        }return super.getDestroySpeed(stack, state);  // 普通速度 1.0
+            return this.getTier().getSpeed()*2f;  // 使用工具本身的挖掘速度
+        }
+        return super.getDestroySpeed(stack, state);  // 普通速度 1.0*/
     }
     @Override
     public boolean isCorrectToolForDrops(ItemStack stack, BlockState state) {
-        return true;  // 永远返回 true，意味着这个工具可以挖任何方块
+        return true;  // 永远返回 true，意味着这个工具可以挖任何方块并使其掉落
     }
+
+
 
 
 }
